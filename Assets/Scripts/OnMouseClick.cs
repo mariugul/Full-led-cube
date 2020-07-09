6184b8845ebfbf478dff6b9fbb4b71aebc15f656
Marius C. K <marius_gulbrandsen@hotmail.no>
2020-07-09 19:27:52 +0200

    Fix clicking LEDs lights them on/off on first click.
    Added a dictionary that stores the value of all LEDs (on/off) to check wether they have been clicked or not. This ensures that the first click is always toggleing the LED depending on it previous status.

--- Assets/Scripts/OnMouseClick.cs
+++ Assets/Scripts/OnMouseClick.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
@@ -8,10 +10,26 @@ public class OnMouseClick : MonoBehaviour
     bool ledOn = false;
     RaycastHit hit;
 
+    // List to store the on or off status of LEDs
+    List<bool> ledStatus = new List<bool>();
+    Dictionary<string, bool> ledStatusDic = new Dictionary<string, bool>();
+
     // Start is called before the first frame update
     void Start()
     {
         MeshCollider mc = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
+
+        // Initialize state of LEDs to list
+        for (int i = 1; i <= 64; i++)
+        {
+            ledStatusDic.Add("led" + i, false);
+        }
+
+        foreach (KeyValuePair<string, bool> led in ledStatusDic)
+        {
+            //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            Console.WriteLine("Key = {0}, Value = {1}", led.Key, led.Value);
+        }
     }
     
     // Update is called once per frame
@@ -29,16 +47,26 @@ public class OnMouseClick : MonoBehaviour
                     // Print name of clicked object
                     PrintName(hit.transform.gameObject);
 
+                    // Save name of clicked LED
+                    string clickedLed = hit.transform.gameObject.name;
+
+                    // Save the status of the LED
+                    bool ledStatus;
+
+                    // Look up the clicked LEDs status in dictionary and save to ledStatus
+                    ledStatusDic.TryGetValue(clickedLed, out ledStatus);
+
                     // Toggle LED light
-                    if (ledOn)
+                    if (ledStatus)
                     {
                         hit.transform.gameObject.transform.GetChild(0).GetComponent<Light>().enabled = false;
-                        ledOn = false;
+                        ledStatusDic[clickedLed] = false;
+                          
                     }
                     else
                     {
                         hit.transform.gameObject.transform.GetChild(0).GetComponent<Light>().enabled = true;
-                        ledOn = true;
+                        ledStatusDic[clickedLed] = true;
                     }
                     
                 }
